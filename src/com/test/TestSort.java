fc06251f90f0e5b6288f216f97c810f4280a1341
dengyouqing <1250273795@qq.com>
2016-08-19 16:09:27 +0800

    完善排序类

--- src/com/test/TestSort.java
+++ src/com/test/TestSort.java
@@ -1,5 +1,19 @@
 package com.test;
 
+import com.louga.util.PrintSort;
+import com.louga.util.Sort;
+
 public class TestSort {
+	public static void main(String[] args){
+		int[] arr={1,3,9,5,8,7,4,2,10,6,0};
+		Sort sort=new Sort();
+		System.out.println("排序前的数组是：");		
+		PrintSort.printsort(arr);
+		
+		int[]arr2=sort.sort(arr);
+		System.out.println("排序后的数组是：");
+		PrintSort.printsort(arr2);
+		
+	}
 
 }
