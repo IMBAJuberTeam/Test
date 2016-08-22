552fcfcfa18d489a6b5324b2ae6f5e9c05de1edc
dengyouqing <1250273795@qq.com>
2016-08-22 09:52:19 +0800

    草了个西瓜

--- src/com/louga/util/Sort2.java
+++ src/com/louga/util/Sort2.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 /*
  * 定义一个类，选择排序
  * 方法名sort2()，输入一个int[]，返回一个int[]
- * 可以理解为实际上是有两个相同的数字，进行比较，第一次拿第一个元素和，第二个数组的所有元素进行比较，然后是
+ * 可以理解为实际上是有两个相同的数组，进行比较，第一次拿第一个元素和第二个数组的所有元素进行比较得到最小值，然后是
  * 第一个数组的第二个元素和第二个数组的所有元素进行比较，这样达到选择排序的效果
  */	 
 public class Sort2 {
@@ -22,7 +22,7 @@ public class Sort2 {
 					 arr[j]=temp;
 				 }
 			}		 	
-			System.out.print(min+" "); 
+			//System.out.print(min+" "); 
 		 }
 		 System.out.print("\n");
 		 return arr;
@@ -32,6 +32,8 @@ public class Sort2 {
 	 public void Test01(){
 		 Sort2 sorttwo=new Sort2();
 		 int[] arr={1,3,9,10,8,6,5,7,4,2};
+		 
+		 System.out.print(Arrays.toString(arr));
 		 System.out.print(Arrays.toString(sorttwo.sort2(arr)));
 	 }
 }
