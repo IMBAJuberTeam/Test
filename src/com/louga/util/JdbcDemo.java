2b90728d46e2aeb2e362a4139eb88e4949f7a8b6
dengyouqing <1250273795@qq.com>
2016-08-22 18:36:24 +0800

    数据库查询字段包含中文的时候查询不到结果，很有可能是由于中文编码的问题导致的（暂未解决）

--- src/com/louga/util/JdbcDemo.java
+++ src/com/louga/util/JdbcDemo.java
@@ -11,21 +11,21 @@ public class JdbcDemo {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//�������ݿ�����
-			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull","root","");
 			
 			//Ϊ���Ӵ���һ��״̬
 			Statement stmt=conn.createStatement();
 			
 			//����һ��sql��䣬��һ�����sqlִ�н������ResultSet����
-			String sql="select * from area where areaName like '%省' ";
-			//String sql="select * from area ";
+			//String sql="select * from area where areaName like \'11%\' ";
+			String sql="select * from area where areaName='%%'";
 			ResultSet rs=stmt.executeQuery(sql);
 			
 			int columns=rs.getMetaData().getColumnCount();
 			while(rs.next()){
 				for(int i=1;i<=columns;i++){
 				    System.out.print(rs.getString(i));
-				    System.out.print("\t\t");
+				    System.out.print("\t");
 				}
 			    System.out.print("\n");
 			}
