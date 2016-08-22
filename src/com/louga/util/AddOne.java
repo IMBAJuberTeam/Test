232ddd3404c86cc0e4a0e7422f8ba920d13fd910
dengyouqing <1250273795@qq.com>
2016-08-22 10:46:27 +0800

    在编写好类的时候不用另外新建测试类，直接在当前类是用@Test重新测试方法即可完成方法的测试工作

--- src/com/louga/util/AddOne.java
+++ src/com/louga/util/AddOne.java
@@ -1,5 +1,7 @@
 package com.louga.util;
 
+import org.junit.Test;
+
 public class AddOne {
 	public void addOne() throws InterruptedException{
 		int x=0;
@@ -8,6 +10,15 @@ public class AddOne {
 			x++;
 			Thread.sleep(1000);
 			
+		}
+	}
+	@Test
+	public void test01(){
+		AddOne addone= new AddOne();
+		try {
+			addone.addOne();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
