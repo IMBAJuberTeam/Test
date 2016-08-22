5dfcd831cf1a26442d386b1455b57cee6d677bb4
dengyouqing <1250273795@qq.com>
2016-08-22 16:57:20 +0800

    数据库测试2

--- src/com/louga/util/JdbcDemo.java
+++ src/com/louga/util/JdbcDemo.java
@@ -1,7 +1,9 @@
 package com.louga.util;
+import java.sql.Statement;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
-import com.mysql.jdbc.*;
 public class JdbcDemo {
 	public static void main(String[] args){
 		try {
@@ -9,11 +11,28 @@ public class JdbcDemo {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//创建数据库连接
-			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/test:3306","root","");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
 			
 			//为连接创建一个状态
 			Statement stmt=conn.createStatement();
-		} catch (ClassNotFoundException e) {
+			
+			//创建一个sql语句，和一个存放sql执行结果集的ResultSet集合
+			String sql="select * from area";
+			ResultSet rs=stmt.executeQuery(sql);
+			
+			//String sql2="update user set password=\"123\" where username=\"dengyouqing\"";
+			//ResultSet rs2=stmt.executeUpdate(sql2);
+			int columns=rs.getMetaData().getColumnCount();
+			while(rs.next()){
+			    for(int i=1;i<=columns;i++){
+			    	System.out.print(rs.getString(i));
+			    	System.out.print("\t\t");
+			    }
+			    System.out.println();
+			}
+			System.out.println(rs);
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
