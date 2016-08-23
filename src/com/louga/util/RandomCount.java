fa5a30f568cb5988569c9de659b62a5678d32b5c
dengyouqing <1250273795@qq.com>
2016-08-23 14:02:25 +0800

    测试代码提交

--- src/com/louga/util/RandomCount.java
+++ src/com/louga/util/RandomCount.java
@@ -1,5 +1,9 @@
 package com.louga.util;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+
 public class RandomCount{
 	public static int[] generator(int s){
 		int[] arr=new int[s];
@@ -7,5 +11,10 @@ public class RandomCount{
 			arr[i]=(int)(Math.random()*100);
 		}
 		return arr;	
+	}
+	@Test
+	public void test01(){
+		RandomCount rc=new RandomCount();
+		System.out.print(Arrays.toString(rc.generator(10)));
 	}
 }
