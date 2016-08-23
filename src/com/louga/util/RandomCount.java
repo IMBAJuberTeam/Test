3c19053fd448d5fc7e41845569e5e13772280db5
dengyouqing <1250273795@qq.com>
2016-08-23 14:07:22 +0800

    Revert "测试代码提交"
    This reverts commit fa5a30f568cb5988569c9de659b62a5678d32b5c.

--- src/com/louga/util/RandomCount.java
+++ src/com/louga/util/RandomCount.java
@@ -1,9 +1,5 @@
 package com.louga.util;
 
-import java.util.Arrays;
-
-import org.junit.Test;
-
 public class RandomCount{
 	public static int[] generator(int s){
 		int[] arr=new int[s];
@@ -11,10 +7,5 @@ public class RandomCount{
 			arr[i]=(int)(Math.random()*100);
 		}
 		return arr;	
-	}
-	@Test
-	public void test01(){
-		RandomCount rc=new RandomCount();
-		System.out.print(Arrays.toString(rc.generator(10)));
 	}
 }
